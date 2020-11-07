598d8631e2ef9cadd8e1e9001abe758f586981fe
Shoaib Ahmed <hussainshoaib46@yahoo.com>
2020-11-07 10:00:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@ import React from 'react'
 import './App.css';
 import './props.css';
 import Routes from './Route'
-// import { BrowserRouter, Route, link } from 'react-router-dom'
-// import HomePage from './components/Home'
-// import Header from './components/Header'
-// import Footer from './components/Footer'
+
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Header />
-    //   <Route path="/" component={HomePage} />
-    //   <Footer />
-    // </BrowserRouter>
     <div>
       <Routes />
     </div>
