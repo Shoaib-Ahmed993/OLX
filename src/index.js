f497b3428986eb94aaa73cde4df6feaa2f43157b
Shoaib Ahmed <hussainshoaib46@yahoo.com>
2020-11-07 19:21:48 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-      </Switch>
-    </Router>
-  </ React.StrictMode>,
+    <App />,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
